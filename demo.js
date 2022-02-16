8d32fca0406b8d316cbbfb6e8c9ceeab3bb4268f
VuTuong1999 <vungoctuong2@gmail.com>
2022-02-16 12:26:37 +0700

    demo-react

--- demo.js
+++ demo.js
@@ -1,2 +1,13 @@
 import React from 'react';
 
+class Aaasds extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Aaasds;
